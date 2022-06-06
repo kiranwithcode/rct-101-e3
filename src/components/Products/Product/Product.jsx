a1523668603288cb4a6f90b5f62c4b47037fd970
kiranwithcode <kirankamble4595@gmail.com>
2022-06-06 17:15:43 +0530

    c4e3

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import { CartContext } from "../../../context/CartContext";
+
 
 const Product = (props) => {
   // Note: this id should come from api
@@ -16,9 +16,9 @@ const Product = (props) => {
 
       })
     },[])
-    // console.log(data.productId)
+ 
   return (
-    <div data-cy={`product-${product.id}`} style={{border:'1px solid black', width:"300px",margin:'auto', borderRadius:"5px"}}>
+    <div data-cy={`product-${product.id}`} style={{border:'1px solid black', width:"250px",margin:'auto', borderRadius:"5px"}}>
       <h3 data-cy="product-name">{product.name}</h3>
       <h6 data-cy="product-description">{product.description}</h6>
       <button data-cy="product-add-item-to-cart-button">{data.count? "Add to Cart":"Add to Cart"}</button>
@@ -29,8 +29,7 @@ const Product = (props) => {
         <span data-cy="product-count">
           {
             data.count
-            // Count here from CartItems
-            // product.count
+           
           }
         </span>
         <button data-cy="product-decrement-cart-item-count-button"
